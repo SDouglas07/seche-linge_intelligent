861c70542237afddbeb8e274681d12badcd2f730
SDouglas07 <sanyldn07@yahoo.com>
2026-05-04 23:13:07 +0100

    Add files via upload

---
+++ smart_dryer/smart_dryer.ino
@@ -0,0 +1,16 @@
+/*Sèche-linge intelligent V2
+*Mini projet L1 GEII - IUT Douala
+*/
+//------Appel des bibliothèques de fonction nécessaires------//
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
